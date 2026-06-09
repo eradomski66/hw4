2d0ccd961103862cccaf827e16c9bea02256e957
eradomski66 <evanspamemail6@gmail.com>
2026-06-09 03:10:35 +0000

    checking if login attempt matches a username and password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:username], password: params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to "/"
+    else
+      flash[:alert] = "Incorrect username or password"
+      redirect_to "/login"
+    end
   end
 
   def destroy
