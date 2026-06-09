c11f23404f6e82c9f049cd1fcc33f1f4d5b4b11d
eradomski66 <evanspamemail6@gmail.com>
2026-06-09 03:00:19 +0000

    coded login tab to have username and password entries

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources "entries"
   resources "places"
   resources "sessions"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources "users"
 end
