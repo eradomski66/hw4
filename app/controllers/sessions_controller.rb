c264b6a3dec3f23be57507a142d00980c7487dbb
eradomski66 <evanspamemail6@gmail.com>
2026-06-09 03:46:04 +0000

    set up new user and login capabilities. still need to differentiate users' posts from each other

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:username], password: params[:password])
+    user = User.find_by(username: params[:username])&.authenticate(params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to "/"
+      redirect_to "/places"
     else
       flash[:alert] = "Incorrect username or password"
       redirect_to "/login"
