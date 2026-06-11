be866ae99454afb20517ec3846550244f96a94c0
eradomski66 <evanspamemail6@gmail.com>
2026-06-11 21:22:31 +0000

    Coded the logged in user  capabilities (e.g. seeing your posts and not others' posts)

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,11 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    if @current_user.nil?
+      redirect_to "/login"
+    else
+      @places = Place.where(user_id: @current_user.id)
+    end
   end
 
   def show
@@ -15,6 +19,7 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new
     @place["name"] = params["name"]
+    @place["user_id"] = @current_user.id
     @place.save
     redirect_to "/places"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_09_032404) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_11_211327) do
   create_table "entries", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_09_032404) do
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
