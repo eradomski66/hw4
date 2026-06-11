45af29d90d7c2dd37d488aa38294106b7684860a
eradomski66 <evanspamemail6@gmail.com>
2026-06-11 21:09:35 +0000

    Changed the links so that they pages include home, login, places

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "places", :action => "index" })
+  get("/", { :controller => "pages", :action => "index" })
   resources "entries"
   resources "places"
   resources "sessions"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  get "/logout", to: "sessions#destroy"
   resources "users"
 end
